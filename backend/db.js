a0f57ee95649d01cc8e25f155a64b74162017147
AdwithGowda <126180447+AdwithGowda@users.noreply.github.com>
2023-12-28 16:00:41 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://AdwithGowda:Ag22187120@adwithgowda.mwyx3lv.mongodb.net/Register_Login?retryWrites=true&w=majority',{
+mongoose.connect('mongoose_URL',{
 })
 .then((response) =>{
     console.log("connected to data base");
